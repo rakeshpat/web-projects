b24ae61b37a1b1f491de2b521589ba45a65d660a
rakesh <32685734+rakeshpat@users.noreply.github.com>
2019-05-22 00:15:30 +0100

    fixed links

--- mysql-crud/index.php
+++ mysql-crud/index.php
@@ -27,10 +27,10 @@
                 <div class="row">
                     <div class="col">
                         <ul class="nav justify-content-center">
-                            <li class="nav-item"><a class="nav-link" href="#">CREATE</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#">READ</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#">UPDATE</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#">DELETE</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#create">CREATE</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#read">READ</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#update">UPDATE</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#delete">DELETE</a></li>
                         </ul>
                     </div>
                 </div>
@@ -73,7 +73,7 @@
                             $obj = new CRUD();
                         </div>
                         <br>
-                        <p>That's it! Now you can use the <a href="#">create</a> <a href="#">read</a> <a href="#">update</a> and <a href="#">delete</a> functions</p>
+                        <p>That's it! Now you can use the <a href="#create">create</a> <a href="#read">read</a> <a href="#update">update</a> and <a href="#delete">delete</a> functions</p>
                     </div>
                 </div>
             </div>
@@ -189,10 +189,18 @@
             </div>
         </section>
 
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 
+        <script>
+            $('a[href*=\\#]:not([href$=\\#])').click(function() {
+                event.preventDefault();
 
+                $('html, body').animate({
+                    scrollTop: $($.attr(this, 'href')).offset().top
+                }, 'slow');
+            });
+        </script>
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
