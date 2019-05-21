434dd4505d24aab3b2b024d8b5cf8079cb3638cd
rakesh <32685734+rakeshpat@users.noreply.github.com>
2019-05-22 00:00:32 +0100

    completed documentation

--- mysql-crud/index.php
+++ mysql-crud/index.php
@@ -27,10 +27,10 @@
                 <div class="row">
                     <div class="col">
                         <ul class="nav justify-content-center">
-                            <li class="nav-item"><a class="nav-link" href="#">create</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#">read</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#">update</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#">delete</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">CREATE</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">READ</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">UPDATE</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">DELETE</a></li>
                         </ul>
                     </div>
                 </div>
@@ -79,17 +79,17 @@
             </div>
         </section>
 
-        <section id="read">
-            <div class="container mt-5">
+        <section id="create">
+            <div class="container pt-5">
                 <div class="row">
                     <div class="col pt-5">
                         <h3>CREATE</h3>
                         <p>Create a record into a table using the <code>insert</code> function.</p>
                         <div class="code">
-                            $obj->insert('table_name', ['field1', 'field2', 'field3'], ['Value 1', 'Testing', 123]);
+                            $obj->insert('customers', ['name', 'address', 'postcode'], ['Joe Bloggs', '123 Fake Street', 'FT12 3FT']);
                         </div>
                         <br>
-                        <p>The example above inserts a record into the table <code>table_name</code>, adding values in the third parameter to the fields in the first parameter.</p>
+                        <p>The example above inserts a record into the table <code>table_name</code>, adding values in the third parameter to the fields in the second parameter.</p>
                         <p>If you have many fields/values to add, you may wish to use a variable to store the arrays and pass them into the function as shown below.</p>
                     </div>
                 </div>
@@ -116,7 +116,7 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col pt-3">
+                    <div class="col pt-3 pb-5">
                         <div class="code">
                             $obj->insert('customers', $fields, $values);
                         </div>
@@ -124,8 +124,70 @@
                 </div>
             </div>
         </section>
-        <section id="update"></section>
-        <section id="delete"></section>
+
+        <section id="read">
+            <div class="container pt-5">
+                <div class="row">
+                    <div class="col pt-5">
+                        <h3>READ</h3>
+                        <p>Reads data from a table using the <code>select</code> function.</p>
+                        <div class="code">
+                            $obj->select('orders', 'order_id', ['customer_name', 'item_desc', 'qty']);
+                        </div>
+                        <br>
+                        <p>The example above selects the fields in the third parameter of the <code>order</code> table.</p>
+                        <p><br><em>Why is the primary key name required in parameter 2?</em></p>
+                        <p>You may require a way of identifying a specific record when the results are obtained. Unique primary key values will allow for any specific record to be captured.</p>
+                        <p><br><em>How do I see the results?</em></p>
+                        <p>A results variable can be used to get the results of the query as shown below.</p>
+                        <div class="code">
+                            $results = $obj->select('contacts', 'contact_id', ['name', 'address', 'contact_no']);
+                        </div>
+                        <br>
+                        <p>Retrieving a value from the table</p>
+                        <div class="code">
+                            echo $results[9]['name'];
+                        </div>
+                        <br>
+                        <p class="pb-5">The example above will output the value in the <code>name</code> field where the <code>contact_id</code> is <code>9</code>.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="update">
+            <div class="container pt-5">
+                <div class="row">
+                    <div class="col pt-5">
+                        <h3>UPDATE</h3>
+                        <p>Updates an existing record using the <code>update</code> function.</p>
+                        <div class="code">
+                            $obj->update('items', ['in_stock'], [0], ['qty', 'supplier'], [0, 'none'];
+                        </div>
+                        <br>
+                        <p>The code in the example above sets all <code>in_stock</code> field values to <code>0</code> where <code>qty</code> is <code>0</code> and <code>supplier</code> is <code>none</code> in the <code>items</code> table.</p>
+                        <p class="pb-5">Parameters 2, 3, 4 and 5 are all arrays and can store many elements. This means you are able to set several values and add several conditions with one statement!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="delete">
+            <div class="container pt-5">
+                <div class="row">
+                    <div class="col pt-5">
+                        <h3>DELETE</h3>
+                        <p>Removes a record from a specified table using the <code>delete</code> function.</p>
+                        <div class="code">
+                            $obj->delete('orders', ['id'], [159]);
+                        </div>
+                        <br>
+                        <p>The above code will remove the record where <code>id</code> is <code>159</code> from the <code>delete</code> table.</p>
+                        <p class="pb-5">Note that the second and third parameters are arrays, so several conditions can be added.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
 
 
 
