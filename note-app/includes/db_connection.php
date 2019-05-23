bc60d9cd2252257e7f63f12c57e3839dfe970452
rakesh <32685734+rakeshpat@users.noreply.github.com>
2019-05-23 01:05:11 +0100

    helper

--- note-app/includes/db_connection.php
+++ note-app/includes/db_connection.php
@@ -1,1 +1,25 @@
 <?php
+
+class DBConnect {
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+
+    public function __construct() {
+        $this->host = 'localhost';
+        $this->username = 'root';
+        $this->password = 'root';
+        $this->database = 'test';
+    }
+
+    public function connect() {
+        $pdo = new PDO(
+            'mysql:host=' . $this->host . ';dbname=' . $this->database,
+            $this->username,
+            $this->password
+        );
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }
+}
